90c80e5c7e0c039744a12ed01515ca3490560991
Jim Viebke <jviebke@gmail.com>
2016-04-04 21:36:37 -0400

    documentation

--- Chess_Engine/board.h
+++ Chess_Engine/board.h
@@ -137,13 +137,13 @@ public:
 
 		board.reserve(64);
 
-		// From Wikipedia:
+		// Edited from Wikipedia:
 
 		// A FEN record contains six fields. The separator between fields is a space. The fields are:
 
-		// 1. Piece placement(from white's perspective). Each rank is described, starting with rank 8 and ending with rank 1; within each rank, the contents of each
+		// 1. Piece placement (from white's perspective). Each rank is described, starting with rank 8 and ending with rank 1; within each rank, the contents of each
 		// square are described from file "a" through file "h". Following the Standard Algebraic Notation (SAN), each piece is identified by a single letter taken
-		// from the standard English names (pawn = "P", knight = "N", bishop = "B", rook = "R", queen = "Q" and king = "K").[1] White pieces are designated using
+		// from the standard English names (pawn = "P", knight = "N", bishop = "B", rook = "R", queen = "Q" and king = "K"). White pieces are designated using
 		// upper-case letters ("PNBRQK") while black pieces use lowercase ("pnbrqk"). Empty squares are noted using digits 1 through 8 (the number of empty squares),
 		// and "/" separates ranks.
 
@@ -163,21 +163,21 @@ public:
 
 		color_to_move = ((strings[1].find('w') != std::string::npos) ? white : black);
 
-		// 3. Castling availability.If neither side can castle, this is "-".Otherwise, this has one or more letters : "K" (White can castle kingside), "Q" (White
-		// can castle queenside), "k" (Black can castle kingside), and / or "q" (Black can castle queenside).
+		// 3. Castling availability. If neither side can castle, this is "-". Otherwise, this has one or more letters: "K" (White can castle kingside), "Q" (White
+		// can castle queenside), "k" (Black can castle kingside), and/or "q" (Black can castle queenside).
 
 		white_can_castle_k_s = (strings[2].find("K") != std::string::npos);
 		white_can_castle_q_s = (strings[2].find("Q") != std::string::npos);
 		black_can_castle_k_s = (strings[2].find("k") != std::string::npos);
 		black_can_castle_q_s = (strings[2].find("q") != std::string::npos);
 
-		// 4. En passant target square in algebraic notation.If there's no en passant target square, this is "-". If a pawn has just made a two-square move, this
-		// is the position "behind" the pawn. This is recorded regardless of whether there is a pawn in position to make an en passant capture.[2]
+		// 4. En passant target square in algebraic notation. If there's no en passant target square, this is "-". If a pawn has just made a two-square move, this
+		// is the position "behind" the pawn. This is recorded regardless of whether there is a pawn in position to make an en passant capture.
 
-		// 5. Halfmove clock : This is the number of halfmoves since the last capture or pawn advance.This is used to determine if a draw can be claimed under the
+		// 5. Halfmove clock. This is the number of halfmoves since the last capture or pawn advance. This is used to determine if a draw can be claimed under the
 		// fifty - move rule.
 
-		// 6. Fullmove number : The number of the full move.It starts at 1, and is incremented after Black's move.
+		// 6. Fullmove number. The number of the full move. It starts at 1, and is incremented after Black's move.
 	}
 
 	static void print_board(const Board & board, const unsigned & offset = 0);
