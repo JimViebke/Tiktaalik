73b13eb8a5cff5c45bac96ba3eec277ca58d8b3a
Jim Viebke <jviebke@gmail.com>
2024-04-16 11:41:39 -0400

    Rewrite perft() and divide()
    Also:
    - remove rank_sort() and generate_ply()

--- Chess_Engine/node.cpp
+++ Chess_Engine/node.cpp
@@ -5,91 +5,80 @@ Mar 28 2016 */
 #include <algorithm>
 
 #include "node.hpp"
+#include "util.hpp"
 
 namespace chess
 {
 	Node::Node(const Board& set_board) : board(set_board) {}
 
-	/*void Node::generate_ply(const unsigned & depth)
+	void Node::perft(const size_t max_depth)
 	{
-		// if we have not reached the desired ply depth
-		if (depth > 0)
-		{
-			// generate child boards for this position
-			const Board::board_list child_boards = get_child_boards();
+		if (max_depth == 0) return;
 
-			// for each child board
-			for (const Board & child_board : child_boards)
-			{
-				// save the child board to its own node
-				child_nodes.emplace_back(child_board);
-
-				// make a recursive call to continue populating down the tree
-				child_nodes.back().generate_ply(depth - 1);
-			}
-		}
-	}*/
-
-	void Node::print_size() const
-	{
-		std::map<size_t, size_t> node_counter;
+		std::map<size_t, size_t> node_counter; // <depth, node count>
 		// hardcode start position
 		node_counter[0] = 1;
 
-		size(node_counter);
+		const auto start_time = util::time_in_ms();
+		perft(1, max_depth, node_counter);
 
-		for (auto it = node_counter.begin(); it != node_counter.end(); ++it)
-			std::cout << "Number of child nodes from ply " << it->first << ": " << it->second << ".\n";
+		std::cout << "Ply:\tNodes:\n";
+		for (auto& counter : node_counter)
+		{
+			std::cout << counter.first << '\t' << counter.second << '\n';
+		}
+
+		std::cout << "(" << util::time_in_ms() - start_time << " ms)\n";
 	}
 
-	/*
-	void Node::divide(const unsigned & depth)
+	void Node::divide(const size_t max_depth)
 	{
-		// generate child boards for this position
-		const Board::board_list child_boards = get_child_boards();
+		if (max_depth < 1)
+		{
+			std::cout << "Divide depth must be at least one.\n";
+			return;
+		}
 
-		// save the child boards to the node
-		for (const Board & child_board : child_boards)
-			child_nodes.emplace_back(child_board);
+		// generate child boards if they don't exist already
+		generate_child_boards();
 
 		size_t total_nodes = 0;
 
-		for (Node & node : child_nodes)
+		for (Node& node : children)
 		{
-			std::map<size_t, size_t> node_counter; // <depth, child count>
-			node.divide(node_counter, depth);
+			std::map<size_t, size_t> node_counter; // <depth, node count>
+			node.perft(1, max_depth - 1, node_counter);
 
-			// print the move
-			std::cout << node.get_move() << " ";
-			if (node_counter.size() > 0)
+			const auto last = node_counter.crbegin();
+
+			if (last != node_counter.crend())
 			{
-				const size_t moves = (----node_counter.end())->second;
-				total_nodes += moves;
-				std::cout << moves << "\n";
+				std::cout << node.board.move_to_string() << ": " << last->second << '\n';
+				total_nodes += last->second;
 			}
-			else
-				std::cout << 0 << "\n";
 		}
 
-		std::cout << "Total nodes: " << total_nodes << std::endl;
+		std::cout << "Total nodes: " << total_nodes << '\n';
 	}
-	*/
 
-	void Node::size(std::map<size_t, size_t>& node_counter, const unsigned& depth) const
+	// perft inner
+	void Node::perft(const size_t depth,
+					 const size_t max_depth,
+					 std::map<size_t, size_t>& node_counter)
 	{
+		generate_child_boards();
+
 		node_counter[depth] += children.size();
-		for (const Node& child : children)
+
+		if (depth < max_depth)
 		{
-			child.size(node_counter, depth + 1);
+			for (auto& child : children)
+			{
+				child.perft(depth + 1, max_depth, node_counter);
+			}
 		}
+
+		// We're not going to use these nodes again; clear them.
+		children.clear();
 	}
-
-	// sort positions by rank instead of file (the default sort)
-	//bool Node::rank_sort(const std::pair<std::string, size_t> & left, const std::pair<std::string, size_t> & right)
-	//{
-	//	if (left.first[1] == right.first[1] && left.first[3] < right.first[3]) return true;
-	//
-	//	return (left.first[1] < right.first[1]);
-	//}
-
 }
--- Chess_Engine/node.hpp
+++ Chess_Engine/node.hpp
@@ -45,30 +45,13 @@ namespace chess
 				children.emplace_back(child_board);
 		}
 
+		void perft(const size_t max_depth);
+		void divide(const size_t max_depth);
+
 	private:
-		// void generate_ply(const unsigned & depth);
-		void print_size() const;
-		// void divide(const unsigned & depth);
-		// static bool Node::rank_sort(const std::pair<std::string, size_t> & left, const std::pair<std::string, size_t> & right);
-
-		void divide(std::map<size_t, size_t>& node_counter, const size_t& max_depth, const size_t& current_depth = 0)
-		{
-			if (current_depth == max_depth) return;
-
-			generate_child_boards();
-
-			// record the number of child boards
-			node_counter[current_depth] += children.size();
-
-			// descend the tree to find the children of each node
-			for (Node& node : children)
-				node.divide(node_counter, max_depth, current_depth + 1);
-
-			// On our way back up, erase all child nodes. This time, we're only storing a maximum of A*D boards,
-			// where A is the average number of child boards per position (~30) and D is the depth.
-			children.clear();
-		}
-
-		void size(std::map<size_t, size_t>& node_counter, const unsigned& depth = 1) const;
+		// inner perft
+		void perft(const size_t depth,
+				   const size_t max_depth,
+				   std::map<size_t, size_t>& node_counter);
 	};
 }
