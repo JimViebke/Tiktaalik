fd70a05bae0089542739ed36f8e59c85d8c06b85
Jim Viebke <jviebke@gmail.com>
2024-06-09 22:04:52 -0400

    Update/clean up inline macros

--- src/config.hpp
+++ src/config.hpp
@@ -21,14 +21,27 @@ namespace chess
 	}
 }
 
-#if !_DEBUG && 1
-#define inline_unspecified
-#define inline_toggle inline
-#define inline_toggle_member inline
-#define force_inline_toggle [[clang::always_inline]] [[maybe_unused]] static
+#if defined __clang__
+#	define force_inline_directive [[clang::always_inline]]
+#	define noinline_directive [[clang::noinline]]
 #else
-#define inline_unspecified [[clang::noinline]] static
-#define inline_toggle [[clang::noinline]] [[maybe_unused]] static
-#define inline_toggle_member [[clang::noinline]]
-#define force_inline_toggle [[clang::noinline]] [[maybe_unused]] static
+#	error "Compiler unrecognized. Add configuration for this compiler."
+#endif
+
+/*
+Some functions are specified as inline, some are specified as force_inline, and some are not specified and left up to the compiler.
+However, for certain kinds of profiling and debugging, we have a number of functions that we want to prevent from inlining.
+Instead of writing these functions as "inline/force_inline/(unspecified)", write these functions as "inline/force_inline/(unspecified), unless specified otherwise".
+In the "otherwise" case, force them to not inline.
+*/
+#if !_DEBUG && 1
+#	define inline_unspecified
+#	define inline_toggle        inline
+#	define inline_toggle_member inline
+#	define force_inline_toggle  force_inline_directive [[maybe_unused]] static
+#else
+#	define inline_unspecified   noinline_directive static
+#	define inline_toggle        noinline_directive [[maybe_unused]] static
+#	define inline_toggle_member noinline_directive
+#	define force_inline_toggle  noinline_directive [[maybe_unused]] static
 #endif
