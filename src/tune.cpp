c365c73741ece94e5cb0c6a8fac7721b2de6c3b3
Jim Viebke <jviebke@gmail.com>
2024-08-18 03:14:29 -0400

    Update tuning method

--- src/tune.cpp
+++ src/tune.cpp
@@ -1,6 +1,4 @@
 
-#include <array>
-#include <charconv>
 #include <fstream>
 #include <iostream>
 #include <random>
@@ -20,9 +18,18 @@ namespace chess
 	const std::string formatted_weights_files = base_dir + "engines/tiktaalik weights formatted.txt";
 	const std::string games_file = base_dir + "games/CCRL-4040.[1913795]-rated-2500-no-mate.uci";
 
-	std::vector<board> test_positions;
-	std::vector<color> side_to_move;
-	std::vector<float> results;
+	struct extended_position
+	{
+		board board;
+		color side_to_move;
+		float result;
+		double error = -2.0;
+	};
+
+	std::vector<extended_position> extended_positions;
+
+	std::vector<extended_position> training_set;
+	std::vector<extended_position> test_set;
 #endif
 
 #if tuning
@@ -201,19 +208,21 @@ namespace chess
 				return;
 			}
 
+			extended_positions.emplace_back();
+
 			// Save the result.
 			const std::string result = *moves.rbegin();
 			if (result == "1-0")
 			{
-				results.push_back(1);
+				extended_positions.back().result = 1;
 			}
 			else if (result == "0-1")
 			{
-				results.push_back(0);
+				extended_positions.back().result = 0;
 			}
 			else if (result == "1/2-1/2")
 			{
-				results.push_back(0.5);
+				extended_positions.back().result = 0.5;
 			}
 			else
 			{
@@ -246,13 +255,13 @@ namespace chess
 				boards[0].verify_key_and_eval(color_to_move);
 			}
 
-			// Save the position, and the color to move.
-			test_positions.push_back(boards[0]);
-			side_to_move.push_back(color_to_move);
+			// Save the position and side to move.
+			extended_positions.back().board = boards[0];
+			extended_positions.back().side_to_move = color_to_move;
 
 			++games_parsed;
 
-			if (games_parsed % 1000 == 0)
+			if (games_parsed % 1'000 == 0)
 			{
 				std::cout << games_parsed << '\n';
 			}
@@ -264,59 +273,53 @@ namespace chess
 #endif
 
 #if tuning
-	static void store_weights_formatted()
+	static void generate_training_and_test_sets(size_t set_size, std::mt19937_64& rng)
 	{
-		std::stringstream ss;
-		ss << "ps_evals = {\n";
-
-		for (size_t i = 0; i < ps_evals.size(); ++i)
+		if (extended_positions.size() == 0)
 		{
-			if (i % 64 == 0)
-			{
-				/*
-				64 * 0 -> "// pawn midgame"
-				64 * 1 -> "// pawn endgame"
-				64 * 2 -> "// knight midgame"
-				64 * 3 -> "// knight endgame"
-				64 * 4 -> "// bishop midgame"
-				64 * 5 -> "// bishop endgame"
-				...
-				64 * 10 -> "// king midgame"
-				64 * 11 -> "// king endgame"
-				*/
-
-				switch (i / 128)
-				{
-				case pawn: ss << "// pawn "; break;
-				case knight: ss << "// knight "; break;
-				case bishop: ss << "// bishop "; break;
-				case rook: ss << "// rook "; break;
-				case queen: ss << "// queen "; break;
-				case king: ss << "// king "; break;
-				}
-
-				if (i % 128 == 0)
-					ss << "midgame:\n";
-				else
-					ss << "endgame:\n";
-			}
-
-			ss << std::setw(5);
-			ss << std::right;
-			ss << int(eval::ps_evals[i]) << ',';
-
-			if (i % 8 == 7) ss << "   //\n";
+			std::cout << "No positions to select from.\n";
+			return;
 		}
 
-		ss << "};\n";
+		if (set_size * 2 > extended_positions.size())
+		{
+			std::cout << "Not enough positions to select unique training and testing positions.\n";
+			return;
+		}
 
-		std::ofstream ofs(formatted_weights_files, std::ios_base::out);
-		ofs << ss.str();
+		const auto start = util::time_in_ms();
+
+		std::cout << std::format("Selecting {} training positions and {} test positions from {} loaded positions... ",
+		    set_size, set_size, extended_positions.size());
+
+		// Make sure we're starting in a clean state.
+		training_set.clear();
+		test_set.clear();
+		training_set.reserve(set_size);
+		test_set.reserve(set_size);
+
+		// Generate indexes 0 through extended_positions.size() - 1.
+		std::vector<uint32_t> indexes;
+		indexes.reserve(extended_positions.size());
+		for (size_t i = 0; i < extended_positions.size(); ++i)
+		{
+			indexes.push_back(i);
+		}
+
+		std::shuffle(indexes.begin(), indexes.end(), rng);
+
+		for (size_t i = 0; i < set_size * 2; i += 2)
+		{
+			training_set.push_back(extended_positions[indexes[i]]);
+			test_set.push_back(extended_positions[indexes[i + 1]]);
+		}
+
+		std::cout << std::format("done ({} ms)\n", util::time_in_ms() - start);
 	}
 #endif
 
 #if tuning
-	/*constexpr*/ double K = 0.4689;
+	/*constexpr*/ double K = 0.7644;
 
 	static_assert(sizeof(double) == 8);
 #endif
@@ -330,63 +333,60 @@ namespace chess
 #endif
 
 #if tuning
-	// Get the mean squared error for a quiescence search of all test positions.
-	static double evaluate()
+	// Get the mean squared error for a quiescence search of all positions in a set.
+	static double evaluate(std::vector<extended_position>& set)
 	{
 		double error_sum = 0.0;
 
-		for (size_t i = 0; i < test_positions.size(); ++i)
+		for (extended_position& ep : set)
 		{
-			boards[0] = test_positions[i];
+			boards[0] = ep.board;
 			boards[0].generate_eval();
 
 			if constexpr (config::verify_key_and_eval)
 			{
-				boards[0].verify_key_and_eval(side_to_move[i]);
+				boards[0].verify_key_and_eval(ep.side_to_move);
 			}
 
-			const eval_t eval = (side_to_move[i] == white) ? alpha_beta<white, true>(0, 0, 0, -eval::mate, eval::mate)
+			const eval_t eval = (ep.side_to_move == white) ? alpha_beta<white, true>(0, 0, 0, -eval::mate, eval::mate)
 			                                               : alpha_beta<black, true>(0, 0, 0, -eval::mate, eval::mate);
 
-			const double error = results[i] - sigmoid((double)eval);
+			const double error = ep.result - sigmoid((double)eval);
 			error_sum += (error * error);
 		}
 
-		return error_sum / test_positions.size();
+		return error_sum / set.size();
 	}
 #endif
 
 #if tuning
 	static void tune_k()
 	{
-		// double best_error = evaluate();
+		double best_error = evaluate(extended_positions);
+		std::cout << std::format("K = {}, error = {}\n", K, best_error);
 
-		//	double adjust = 1.0;
-		//	while (adjust > 0.000000000000001)
-		//	{
-		//		for (const double delta : {adjust, -adjust})
-		//		{
-		//			K += delta;
-		//			double new_error = evaluate();
-
-		//			if (new_error < best_error)
-		//			{
-		//				best_error = new_error;
-		//				std::cout << "K = " << K << ", error = " << best_error << '\n';
-		//				break;
-		//			}
-
-		//			K -= delta;
-		//		}
-
-		//		adjust /= 2;
-		//	}
-
-		for (int i = 4600; i <= 4800; ++i)
+		double adjust = 1;
+		while (adjust > 0.000000000000001)
 		{
-			K = double(i) / 10'000.0;
-			std::cout << std::setprecision(15) << "( " << K << ", " << evaluate() << ")\n";
+			for (const double delta : {adjust, -adjust})
+			{
+				K += delta;
+				double new_error = evaluate(extended_positions);
+
+				if (new_error < best_error)
+				{
+					best_error = new_error;
+					std::cout << std::format("Improved K to {}, error = {} (adjust = {})\n", K, best_error, adjust);
+					break;
+				}
+
+				K -= delta;
+			}
+
+			adjust /= 2;
 		}
+
+		std::cout << "Finished tuning K.\n";
 	}
 #endif
 
@@ -395,36 +395,65 @@ namespace chess
 	{
 		std::cout << "Tuning piece-square evals.\n";
 
-		double best_error = evaluate();
-		std::cout << std::format("Starting error: {:1.9f}\n", best_error);
+		std::mt19937_64 rng{0x0123456789abcdef};
 
-		bool improving = true;
-		while (improving)
+		double test_error_before{};
+		double test_error_after{};
+
+		const auto start = util::time_in_ms();
+
+		size_t pass = 1;
+		do
 		{
-			improving = false;
+			const auto pass_start = util::time_in_ms();
+
+			store_weights();
+			store_weights_formatted();
+
+			generate_training_and_test_sets(100'000, rng);
+
+			// Evaluate both sets using the existing weights.
+			// We will be tuning using the training set, and keeping the changes.
+			const double training_error_before = evaluate(training_set);
+			test_error_before = evaluate(test_set);
+			double training_error_after = training_error_before;
+
 			for (size_t i = 0; i < eval::ps_evals.size(); ++i)
 			{
-				for (int8_t delta : {1, -1})
+				for (int8_t delta : {5, -5})
 				{
 					eval::ps_evals[i] += delta;
 
-					const double new_error = evaluate();
-					if (new_error < best_error)
+					const double new_error = evaluate(training_set);
+					if (new_error < training_error_after)
 					{
-						store_weights();
-						store_weights_formatted();
-						std::cout << std::format(
-						    "Improved ps_evals[{:>3}] from {:>4} to {:>4}. Old error: {:1.9f}, new error: {:1.9f}\n", i,
-						    int(eval::ps_evals[i] - delta), int(eval::ps_evals[i]), best_error, new_error);
-						best_error = new_error;
-						improving = true;
+						std::cout << std::format("Improved ps_evals[{:>3}] from {:>4} to {:>4}. New error: {:1.9f}\n",
+						    i, int(eval::ps_evals[i] - delta), int(eval::ps_evals[i]), training_error_after);
+						training_error_after = new_error;
 						break;
 					}
 
 					eval::ps_evals[i] -= delta;
 				}
 			}
-		}
+
+			test_error_after = evaluate(test_set);
+
+			const auto now = util::time_in_ms();
+
+			std::stringstream ss;
+			ss << std::format("Finished pass {} in {} ms ({} minutes elapsed).\n", pass++, now - pass_start,
+			    (now - start + 30'000) / 60'000);
+			ss << std::format("Training set error before: {:1.9f}\n", training_error_before);
+			ss << std::format("Training set error after:  {:1.9f} ({:+1.9f})\n", training_error_after,
+			    training_error_after - training_error_before);
+			ss << std::format("Test set error before:     {:1.9f}\n", test_error_before);
+			ss << std::format("Test set error after:      {:1.9f} ({:+1.9f})", test_error_after,
+			    test_error_after - test_error_before);
+
+			util::log(ss.str());
+			std::cout << ss.str() << '\n';
+		} while (test_error_after <= test_error_before);
 
 		std::cout << "Finished tuning.\n";
 	}
